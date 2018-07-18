b6e2004bea2bb3aebd19a5acff4c2a30330ed4ff
paranjapeved <paranjapeved@gmail.com>
2018-07-18 17:22:46 -0400

    Initial Commit

---
+++ create_embeddings.py
@@ -0,0 +1,85 @@
+from os import listdir
+from pickle import dump
+import keras
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+
+def extract_features(directory,target_size):
+
+	#model = keras.applications.inception_v3.InceptionV3(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
+	model = VGG16()
+	#Modify model to remove the last layer
+	model.layers.pop()
+	model = Model(inputs=model.inputs,outputs=model.layers[-1].output)
+	print(model.summary())
+
+	for sub_dir in listdir(directory):
+		if sub_dir == '.DS_Store':
+			continue
+
+		if sub_dir == 'ImposterRaw':
+
+			features = {}
+			subdirectory = directory + "/"+ sub_dir
+
+			for subsub in listdir(subdirectory):
+				if subsub == ".DS_Store":
+					continue
+
+				subsubdirectory = subdirectory + "/" + subsub
+
+				for img_name in listdir(subsubdirectory):
+					filename = subsubdirectory+ "/" + img_name
+					if img_name == 'Thumbs.db' or img_name == '.DS_Store':
+						continue
+
+					image = load_img(filename,target_size=(target_size,target_size))
+					image = img_to_array(image)
+
+					image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+					# prepare the image for the VGG model
+					image = preprocess_input(image)
+					# get features
+					img_feature = model.predict(image, verbose=0)
+					# store feature
+					features[subsub+"\\"+img_name] = img_feature
+					print('>%s' % filename)
+			dump(features, open("ImposterRaw" + ".pkl", 'wb'))
+
+		if sub_dir == 'ClientRaw':
+
+			features = {}
+			subdirectory = directory + "/" + sub_dir
+
+			for subsub in listdir(subdirectory):
+				if subsub == ".DS_Store":
+					continue
+
+				subsubdirectory = subdirectory + "/" + subsub
+
+				for img_name in listdir(subsubdirectory):
+					filename = subsubdirectory+ "/" + img_name
+					if img_name == 'Thumbs.db' or img_name == '.DS_Store':
+						continue
+
+					image = load_img(filename, target_size=(target_size, target_size))
+					image = img_to_array(image)
+
+					image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+					# prepare the image for the VGG model
+					image = preprocess_input(image)
+					# get features
+					img_feature = model.predict(image, verbose=0)
+					# store feature
+					features[subsub + "\\" + img_name] = img_feature
+					print('>%s' % filename)
+			dump(features, open("ClientRaw"+".pkl", 'wb'))
+
+
+
+img_features = extract_features("raw",224)
+
+
---
+++ model.py
@@ -0,0 +1,146 @@
+from sklearn import svm
+import pickle as pkl
+import numpy as np
+import random
+import keras
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+from sklearn.metrics import confusion_matrix
+
+def load_file(filename):
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+def load_features(filename,dataset):
+    all_features = pkl.load(open(filename, 'rb'))
+    # filter features
+
+    features = []
+    for j in dataset:
+        # for arr,_ in all_features[j]:
+        # print(all_features[j][0])
+        try:
+            features.append(list(all_features[j][0]))
+        except:
+            continue
+
+    # unzipped = []
+    # for i in features:
+    #     print(i)
+    #
+    #     unzipped.append(i)
+    # print(unzipped)
+
+    return features
+
+def train_svm(feature_vector,labels):
+    model = svm.SVC()
+    model.fit(feature_vector,labels)
+    return model
+
+def predict_image(svm_model,path,target_size=224):
+    model = VGG16()
+    # Modify model to remove the last layer
+    model.layers.pop()
+    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+
+    image = load_img(path, target_size=(target_size, target_size))
+    image = img_to_array(image)
+
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    image = preprocess_input(image)
+    # get features
+    img_feature = model.predict(image, verbose=0)
+    return svm_model.predict(list(img_feature))
+
+
+
+
+if __name__ == '__main__':
+    positive_train = load_file('client_train_raw.txt')
+    positive_train = positive_train.split('\n')
+    positive_train = positive_train[0:-1]
+    negative_train = load_file('imposter_train_raw.txt')
+    negative_train = negative_train.split('\n')
+    negative_train = negative_train[0:-1]
+
+
+    positive_features = load_features('ClientRaw.pkl',positive_train)
+    negative_features = load_features('ImposterRaw.pkl',negative_train)
+
+
+    # print(positive_features)
+
+    print("training non-spoofed images:", len(positive_features))
+    print("training spoofed_images:", len(negative_features))
+
+    positive_labels = list(np.zeros((len(positive_features)),int))
+    negative_labels = list(np.ones((len(negative_features)),int))
+    for i in range(len(negative_features)):
+        positive_features.append(negative_features[i])
+        positive_labels.append(negative_labels[i])
+
+    combined = list(zip(positive_features, positive_labels))
+    random.shuffle(combined)
+
+    positive_features[:], positive_labels[:] = zip(*combined)
+    print(len(positive_features[0]))
+
+
+    # X = [[0, 0], [1, 1],[0,1]]
+    # y = [0, 1,0]
+
+
+    model = train_svm(positive_features,positive_labels)
+    print(model)
+
+    positive_test = load_file('client_test_raw.txt')
+    positive_test = positive_test.split('\n')
+    positive_test = positive_test[0:-1]
+    positive_features_test = load_features('ClientRaw.pkl',positive_test)
+    negative_test = load_file('imposter_test_raw.txt')
+    negative_test = negative_test.split('\n')
+    negative_test = negative_test[0:-1]
+    negative_features_test = load_features('ImposterRaw.pkl', negative_test)
+    # print(positive_features_test[0])
+    print("Test images not spoofed:",len(positive_features_test))
+    print("Test images spoofed:",len(negative_features_test))
+    predictions = []
+
+    positive_labels_test = list(np.zeros((len(positive_features_test)), int))
+    negative_labels_test = list(np.ones((len(negative_features_test)), int))
+    for i in range(len(negative_features_test)):
+        positive_features_test.append(negative_features_test[i])
+        positive_labels_test.append(negative_labels_test[i])
+
+    combined = list(zip(positive_features_test, positive_labels_test))
+    random.shuffle(combined)
+
+    positive_features_test[:], positive_labels_test[:] = zip(*combined)
+
+    print("Now predicting...")
+    for img in positive_features_test:
+
+        predictions.append(model.predict([img])[0])
+
+    tn, fp, fn, tp = confusion_matrix(list(positive_labels_test),predictions).ravel()
+
+    print("TP:",tp)
+    print("TN:",tn)
+    print("FP:",fp)
+    print("FN:",fn)
+
+    print("Accuracy:",(tp+tn)/(tp+tn+fp+fn))
+
+    # print("prediction:",predict_image(model,'non.jpeg'))
+
+
